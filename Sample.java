21e2ad7b588b65e7a41ae1ff75da670e348461fa
hari442 <85391837+hari442@users.noreply.github.com>
2021-06-19 11:04:38 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,10 +4,10 @@ public static void main(String[] args)
 {
 System.out.println("Hello world!");
 System.out.println("Welcome");
-<<<<<<< HEAD
+
 System.out.println("Hari");
-=======
+
 System.out.println("Venkat");
->>>>>>> cf119fb67c3c279e07690c720f0cfb0f949bbd27
+
 }
 }
