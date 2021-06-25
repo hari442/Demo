b9225820b74b881cb78f0305510deb06945032db
harivenkatasivasai1999@gmail.com <harivenkatasivasai1999@gmail.com>
2021-06-25 14:36:11 +0530

    Added sai

--- Sample.java
+++ Sample.java
@@ -5,9 +5,10 @@ public static void main(String[] args)
 System.out.println("Hello world!");
 System.out.println("Welcome");
 
-System.out.println("Hari");
+System.out.println("Hari ");
 
 System.out.println("Venkat");
 
+System.ouot.println("Sai");
 }
 }
